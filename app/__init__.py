5a0452bd36d7e0d795b986b91adbad54c196a375
Steve Herzog <herzogdev@gmail.com>
2021-03-06 15:56:59 -0600

    feat: add /metrics endpoint that accepts a GET request with a "metric_value" parameter and returns all days when any existing artist 'crossed' that metric value

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,36 @@ def create_app(config_class: object):
 
     @app.route("/metrics", methods=["GET"])
     def metrics() -> List:
-        """IMPLEMENT YOUR SOLUTION HERE."""
+        """Return list of artists that crossing the metric value."""
+
+        # Get metric_value params or set default to 0
+        metric_value = request.args.get('metric_value', 0) or 0
+
+        # Fetch all metrics
+        metrics = Metric.query.order_by(Metric.artist_id.asc(), Metric.date.asc()).all()
+
+        # Find the days when any artist crossed the passed value
+        artist_metrics = {}
+        yesterday_metrics = {}
+
+        for metric in metrics:
+            if not artist_metrics.get(metric.artist_id):
+                artist_metrics[metric.artist_id] = []
+
+            if metric.value >= int(metric_value) and (not artist_metrics[metric.artist_id] or yesterday_metrics[metric.artist_id] < int(metric_value)):
+                artist_metrics[metric.artist_id].append(metric.date.strftime('%Y-%m-%d'))
+            
+            yesterday_metrics[metric.artist_id] = metric.value
+
+        # Return a list of all artists as dictionaries with the artist id and all "crossings"
+        artist_crossings = [
+            {
+                'artist_id': artist_id,
+                'crossings': crossings
+            }
+            for artist_id, crossings in artist_metrics.items()
+        ]
+
+        return jsonify(artist_crossings)
 
     return app
